741e9e1b69ba787bb6192c6d856d38632f33f89e
mete karagöz <metekrgz2000@gmail.com>
2022-11-04 10:45:03 +0300

    04.11.2022

--- WebAPI/Controllers/CarsController.cs
+++ WebAPI/Controllers/CarsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Business.Concrete;
@@ -27,6 +28,8 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
+
+            Thread.Sleep(5000);
 
             var result = _carService.GetAll();
             if (result.Success)
